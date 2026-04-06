9618075ddb5c83af51ced19a3b13132a719a37a0
kaito92i <kaitojb6@gmail.com>
2026-04-06 23:50:36 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,11 +2,11 @@ import tkinter as tk
 from main import lancer_jeu
 
 racine = tk.Tk() # Création de la fenêtre racine
-label = tk.Label(racine, text="Jeu de la fourmis",
+label = tk.Label(racine, bg = "blue", fg = "cyan", text="Jeu de la fourmis",
                   padx=20, pady=20, font = ("helvetica", "30") 
                 )
 label.grid(row=0, column=0)
-bouton = tk.Button(racine, text="play", 
+bouton = tk.Button(racine, bg = "cyan", fg = "green", text="play", 
                     command=lancer_jeu, font = ("helvetica", "30") 
                   ) # création du button
 bouton.grid(row=1, column=0) # positionnement du button
