653cbf5e0a68b75d6cf590c6fa4d88e7ee2dc709
kaito92i <kaitojb6@gmail.com>
2026-05-03 16:08:51 +0200

    Update main.py

--- Projet fourmi/main.py
+++ Projet fourmi/main.py
@@ -15,16 +15,32 @@ def lancer_jeu():
 
     pos_ant = (r.randint(0, height // size - 1), r.randint(0, width // size - 1))
     ant = Ant(pos_ant, root, size, height, width, matrice)
+    paused = False  # état pause
+
+    def toggle_pause():
+        nonlocal paused
+        paused = not paused
+        pause_bouton.config(text="Reprendre" if paused else "Pause")
+
 
     def game_loop():
+        nonlocal paused
+
+        if paused:
+            root.after(500, game_loop)
+            return 
         old_pos = (ant.p[0], ant.p[1])
         ant.move()
         update_cell(old_pos[0], old_pos[1])
         ant.draw()
         root.after(500, game_loop)
 
+    pause_bouton = tk.Button( root, text="Pause",bg="red", fg="black",
+                       font=("Helvetica", 20, "bold"), command=toggle_pause)
+    pause_bouton.place(x=10, y=10)
+
     game_loop()
     root.mainloop()
 
-if __name__ == "__main__":  # ← cette ligne est la clé
+if __name__ == "__main__": 
     lancer_menu()
